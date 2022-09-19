1762fddafcda542146c67e8010aa520fba06c8ef
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-09-19 17:29:22 +0900

    Design

--- kmws-ui/pages/index.tsx
+++ kmws-ui/pages/index.tsx
@@ -8,46 +8,93 @@ const Home: NextPage = () => {
       <Head>
         <title>K&amp;M Web Services</title>
       </Head>
-
-      <header>
-        <h1>K&amp;M Web Services</h1>
+      <header className={styles.header}>
+        <h1 className={styles.headerLogo}>K&amp;M Web Services</h1>
       </header>
-
       <main className={styles.main}>
         <h2>Accounting</h2>
-        <table>
-          <tr>
-            <th>Date</th>
-            <th>Place</th>
-            <th>Payer</th>
-            <th>Item</th>
-            <th>Amount</th>
-            <th>Actions</th>
-          </tr>
-          <tr>
-            <td>2022-01-01</td>
-            <td>Tokyo</td>
-            <td>Taro</td>
-            <td>Juice</td>
-            <td>100</td>
-            <td><button>Delete</button></td>
-          </tr>
+        <h3 className={styles.serviceLogo}>Payments</h3>
+        <table className={styles.dataTable}>
+          <thead>
+            <tr>
+              <th>Date</th>
+              <th>Place</th>
+              <th>Payer</th>
+              <th>Item</th>
+              <th>Amount</th>
+              <th>Actions</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2022-01-01</td>
+              <td>Tokyo</td>
+              <td>Taro</td>
+              <td>Juice</td>
+              <td>100</td>
+              <td><button className={styles.actionButton}>Delete</button></td>
+            </tr>
+            <tr>
+              <td>2022-01-01</td>
+              <td>Tokyo</td>
+              <td>Taro</td>
+              <td>Juice</td>
+              <td>100</td>
+              <td><button className={styles.actionButton}>Delete</button></td>
+            </tr>
+            <tr>
+              <td>2022-01-01</td>
+              <td>Tokyo</td>
+              <td>Taro</td>
+              <td>Juice</td>
+              <td>100</td>
+              <td><button className={styles.actionButton}>Delete</button></td>
+            </tr>
+          </tbody>
         </table>
-
+        <h3>Sharing</h3>
+        <table className={styles.dataTable}>
+          <thead>
+            <tr>
+              <th>Month</th>
+              <th>Taro Paid</th>
+              <th>Hanako Paid</th>
+              <th>Taro's Share</th>
+              <th>Hanako's Share</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2022-01</td>
+              <td className={styles.numberCell}>100</td>
+              <td className={styles.numberCell}>100</td>
+              <td className={styles.numberCell}>-200</td>
+              <td className={styles.numberCell}>200</td>
+              <td>Adjusted</td>
+            </tr>
+          </tbody>
+        </table>
         <h3>History</h3>
-        <table>
-          <tr>
-            <th>Timestamp</th>
-            <th>Editor</th>
-            <th>Data</th>
-            <th>Action</th>
-          </tr>
-          <tr>
-            <td>2022-01-01T00:00:00+09</td>
-            <td>Taro</td>
-            <td>2022-01-01/Tokyo/Juice/100</td>
-            <td>Add</td>
-          </tr>
+        <table className={styles.dataTable}>
+          <thead>
+            <tr>
+              <th>Timestamp</th>
+              <th>Editor</th>
+              <th>Action</th>
+              <th>Before</th>
+              <th>After</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>2022-01-01T00:00:00+09</td>
+              <td>Taro</td>
+              <td>Add</td>
+              <td></td>
+              <td>Payments/2022-01-01/Tokyo/Juice/100</td>
+            </tr>
+          </tbody>
         </table>
       </main>
     </div>
