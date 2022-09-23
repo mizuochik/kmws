0db10371a98d6b239bb0af8349cf60a7af573d34
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-09-23 22:14:23 +0900

    Fix

--- kmws-accounting/setup.py
+++ kmws-accounting/setup.py
@@ -1,23 +1,25 @@
 from distutils.core import setup
 
-setup(name='kmws-accounting',
-      version='0.0',
-      author='Mizuochi Keita',
-      author_email='keitam913@yahoo.co.jp',
-      packages=[
-          'kmws_accounting',
-          'kmws_accounting.application',
-          'kmws_accounting.adapters',
-      ],
-      package_data={
-          'kmws_accounting': [
-              'py.typed',
-          ],
-          'kmws_accounting.adapters': [
-              'schema.graphql',
-          ],
-      },
-      install_requires=[
-          'ariadne==0.16.0',
-          'uvicorn==0.18.3',
-      ])
+setup(
+    name="kmws-accounting",
+    version="0.0",
+    author="Mizuochi Keita",
+    author_email="keitam913@yahoo.co.jp",
+    packages=[
+        "kmws_accounting",
+        "kmws_accounting.application",
+        "kmws_accounting.adapters",
+    ],
+    package_data={
+        "kmws_accounting": [
+            "py.typed",
+        ],
+        "kmws_accounting.adapters": [
+            "schema.graphql",
+        ],
+    },
+    install_requires=[
+        "ariadne==0.16.0",
+        "uvicorn==0.18.3",
+    ],
+)
