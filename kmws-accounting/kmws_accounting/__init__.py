9e8816c0972f7384df16066374a5320375aab386
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-09-30 03:02:37 +0900

    Add

--- kmws-accounting/kmws_accounting/__init__.py
+++ kmws-accounting/kmws_accounting/__init__.py
@@ -1,6 +1,13 @@
-from .adapters import graphql, dynamodb
+from .adapters import graphql, dynamodb, env
+from starlette.middleware.cors import CORSMiddleware
 
 _payment_event_dao = dynamodb.PaymentEventDao("TestKmwsAccounting")
 _payment_dao = dynamodb.PaymentDao(_payment_event_dao)
 
-app = graphql.make_graphql_app(_payment_dao, _payment_event_dao)
+_config = env.Config.load()
+
+app = CORSMiddleware(
+    graphql.make_graphql_app(_payment_dao, _payment_event_dao),
+    allow_methods=["GET", "POST"],
+    allow_origins=[_config.kmws_ui_origin],
+)
