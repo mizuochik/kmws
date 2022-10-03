e88ca6e4da4b6db2cfe4f525182bffda2fccf845
Mizuochi Keita <keitam913@yahoo.co.jp>
2022-10-04 01:02:50 +0900

    Add

--- kmws-accounting/kmws_accounting/adapters/env.py
+++ kmws-accounting/kmws_accounting/adapters/env.py
@@ -1,6 +1,7 @@
 from __future__ import annotations
 from dataclasses import dataclass
 import os
+from kmws_accounting.application.model import PaymentRatio
 
 TEST = "test"
 
@@ -8,7 +9,16 @@ TEST = "test"
 @dataclass
 class Config:
     kmws_ui_origin: str
+    payment_ratio: PaymentRatio
 
     @classmethod
     def load(cls) -> Config:
-        return Config(kmws_ui_origin=os.environ["KMWS_UI_ORIGIN"])
+        ratio = PaymentRatio()
+        for sec in os.environ["PAYMENT_RATIO"].split(","):
+            k, v = sec.split(":")
+            ratio[k] = int(v)
+
+        return Config(
+            kmws_ui_origin=os.environ["KMWS_UI_ORIGIN"],
+            payment_ratio=PaymentRatio(ratio),
+        )
--- kmws-accounting/tests/adapters/test_env.py
+++ kmws-accounting/tests/adapters/test_env.py
@@ -1,14 +1,19 @@
 from unittest import mock
 import os
 from kmws_accounting.adapters.env import Config
+from kmws_accounting.application.model import PaymentRatio
 
 
 @mock.patch.dict(
     os.environ,
     {
         "KMWS_UI_ORIGIN": "http://xxx.yyy",
+        "PAYMENT_RATIO": "taro:60,hanako:40",
     },
 )
 def test_config_load():
     c = Config.load()
-    assert c == Config(kmws_ui_origin="http://xxx.yyy")
+    assert c == Config(
+        kmws_ui_origin="http://xxx.yyy",
+        payment_ratio=PaymentRatio({"taro": 60, "hanako": 40}),
+    )
